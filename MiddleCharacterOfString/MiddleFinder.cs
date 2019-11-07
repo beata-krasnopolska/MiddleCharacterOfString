57a375e45b420c7271cdd209c2ba93b3bbc0526d
beata.krasnopolska@gmail.com <beata.krasnopolska@gmail.com>
2019-11-07 08:23:56 +0100

    Add project files.

---
+++ MiddleCharacterOfString/MiddleFinder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MiddleCharacterOfString
+{
+    class MiddleFinder
+    {
+        public string FindMiddle(string userString)
+        {
+            string middleString = "";
+            if (string.IsNullOrEmpty(userString))
+            {
+                middleString = "empty string";
+            }
+            else if (userString.Length < 2)
+            {
+                middleString = userString.Substring((userString.Length - 1) / 2, Math.Min(userString.Length,2));
+            }
+            else if(userString.Length %2 == 0)
+            {
+                middleString = userString.Substring((userString.Length - 1) / 2, 2);
+            }
+            else if (userString.Length % 2 != 0)
+            {
+                middleString = userString.Substring((userString.Length - 1) / 2, 1);
+            }
+            return middleString;
+        }
+    }
+}
---
+++ MiddleCharacterOfString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MiddleCharacterOfString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program takes a string (single word) and returns the middle character(s). If the word's length is odd, return the middle character. If the word's length is even, return the middle two characters.");
+
+            Console.WriteLine("Enter a word:");
+            string userWord = Console.ReadLine();
+
+            var middleFinder = new MiddleFinder();
+            var middleLetters = middleFinder.FindMiddle(userWord);
+
+            Console.WriteLine(middleLetters);
+
+            Console.ReadKey();
+        }
+    }
+}
